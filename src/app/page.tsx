d127c4f92f74cc23da7860ebffb2c5352461fbb5
assyifaptrs <muliaassyifa@gmail.com>
2024-07-29 14:21:22 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center bg-white">
+      <div className="w-full"></div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+      },
+      colors: {
+        blue: '#38B9CE',
+        white: '#FCFCFC',
+        black: '#1C1C1C',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
