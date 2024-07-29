0eac6f317b90bf8d63810e763499b4e56dbfcb6d
assyifaptrs <muliaassyifa@gmail.com>
2024-07-29 14:27:24 +0700

    Import components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Hero from '@/components/Hero';
+import About from '@/components/About';
 
 const Page = () => {
   return (
     <div className="flex justify-center items-center bg-white">
-      <div className="w-full"></div>
+      <div className="w-full">
+        <Hero />
+        <About />
+      </div>
     </div>
   );
 };
